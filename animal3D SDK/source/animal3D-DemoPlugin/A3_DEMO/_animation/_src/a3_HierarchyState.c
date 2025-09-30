91bd79f6c77d7e705704501cd04c2c3215ff229d
Benjamin Trudell <benjamin.trudell@mymail.champlain.edu>
2025-09-30 00:02:01 -0400

    Header reading works
    Throwing an exception rn but it's prob cause other stuff isn't implemented yet

--- animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_HierarchyState.c
+++ animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_HierarchyState.c
@@ -299,8 +299,6 @@ a3byte calculateValue(FILE* file, char* value, char* line)
 		return true;
 	}
 
-	currLine[strcspn(currLine, "\n")] = "\0";
-
 	if (currLine[0] == '[' || currLine[0] == '#')
 	{
 		return false;
@@ -321,6 +319,8 @@ a3byte handleHeaders(a3_Hierarchy* hierarchy_out, a3_HierarchyPoseGroup* poseGro
 	{
 		if (strstr(line, "FileType"))
 		{
+			printf("%s\n", value);
+
 			if (!strstr(value, "HTR"))
 			{
 				return false;
@@ -328,6 +328,8 @@ a3byte handleHeaders(a3_Hierarchy* hierarchy_out, a3_HierarchyPoseGroup* poseGro
 		}
 		else if (strstr(line, "DataType"))
 		{
+			printf("%s\n", value);
+
 			if (!strstr(value, "HTRS"))
 			{
 				return false;
@@ -335,6 +337,8 @@ a3byte handleHeaders(a3_Hierarchy* hierarchy_out, a3_HierarchyPoseGroup* poseGro
 		}
 		else if (strstr(line, "FileVersion"))
 		{
+			printf("%s\n", value);
+
 			if (atoi(value) != 1)
 			{
 				return false;
@@ -342,14 +346,20 @@ a3byte handleHeaders(a3_Hierarchy* hierarchy_out, a3_HierarchyPoseGroup* poseGro
 		}
 		else if (strstr(line, "NumSegments"))
 		{
+			printf("%s\n", value);
+
 			a3hierarchyCreate(hierarchy_out, (a3ui32)atoi(value), NULL);
 		}
 		else if (strstr(line, "NumFrames"))
 		{
+			printf("%s\n", value);
+
 			a3hierarchyPoseGroupCreate(poseGroup_out, hierarchy_out, (a3ui32)atoi(value));
 		}
 		else if (strstr(line, "EulerRotationOrder"))
 		{
+			printf("%s\n", value);
+
 			if (strstr(line, "ZYX"))
 			{
 				*poseGroup_out->order = a3poseEulerOrder_zyx;
@@ -357,6 +367,8 @@ a3byte handleHeaders(a3_Hierarchy* hierarchy_out, a3_HierarchyPoseGroup* poseGro
 		}
 		else if (strstr(line, "CalibrationUnits"))
 		{
+			printf("%s\n", value);
+
 			if (strstr(value, "mm"))
 			{
 				unitScale = 1.0f / 10.0f;
@@ -364,6 +376,8 @@ a3byte handleHeaders(a3_Hierarchy* hierarchy_out, a3_HierarchyPoseGroup* poseGro
 		}
 		else if (strstr(line, "BoneLengthAxis"))
 		{
+			printf("%s\n", value);
+
 			if (strstr(value, "Y"))
 			{
 				*poseGroup_out->channel = a3poseChannel_scale_y;
@@ -371,6 +385,8 @@ a3byte handleHeaders(a3_Hierarchy* hierarchy_out, a3_HierarchyPoseGroup* poseGro
 		}
 		else if (strstr(line, "ScaleFactor"))
 		{
+			printf("%s\n", value);
+
 			scaleFactor = (a3f32)atof(value);
 		}
 	}
@@ -387,9 +403,12 @@ a3byte handleSegmentsAndHierarchy(a3_Hierarchy* hierarchy_out, FILE* file)
 	a3i32 childIndex = 0;
 	a3i32 parentIndex = 0;
 
+	printf("\nSegments and Hierarchy\n");
+
 	while (fgets(line, sizeof(line), file) && line[0] != '[' && line[0] != '#')
 	{
-		sscanf(line, "%s %s", &child, &parent);
+		//sscanf(line, "%s %s", &child, &parent);
+		printf("%s %s\n", child, parent);
 
 		parentIndex = a3hierarchyGetNodeIndex(hierarchy_out, parent);
 
@@ -403,7 +422,8 @@ a3byte handleSegmentsAndHierarchy(a3_Hierarchy* hierarchy_out, FILE* file)
 
 a3byte handleBasePosition()
 {
-	char line[512];
+	//char line[512];
+
 
 	return true;
 }
@@ -431,7 +451,7 @@ a3i32 a3hierarchyPoseGroupLoadHTR(a3_HierarchyPoseGroup* poseGroup_out, a3_Hiera
 			return -1;			
 		}
 
-		while (!fgets(line, sizeof(line), file))
+		while (fgets(line, sizeof(line), file))
 		{
 			if (strstr(line, "[Header]"))
 			{
@@ -440,22 +460,22 @@ a3i32 a3hierarchyPoseGroupLoadHTR(a3_HierarchyPoseGroup* poseGroup_out, a3_Hiera
 					return -1;
 				}
 			}
-
-			if (strstr(line, "[Segments&Hierarchy]"))
-			{
-				if (!handleSegmentsAndHierarchy(hierarchy_out, file))
-				{
-					return -1;
-				}
-			}
-
+		
+			//if (strstr(line, "[Segments&Hierarchy]"))
+			//{
+			//	if (!handleSegmentsAndHierarchy(hierarchy_out, file))
+			//	{
+			//		return -1;
+			//	}
+			//}
+		
 			if (strstr(line, "[BasePosition]"))
 			{
-
+		
 			}
 		}
-		
-		fclose(file);		
+
+		fclose(file);
 
 //-----------------------------------------------------------------------------
 //****END-TO-DO-PROJECT-2
